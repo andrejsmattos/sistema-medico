bfbbe55e2649dc530941bd0c462c402a5d9773ac
André <andrejusmattos@gmail.com>
2024-07-17 10:24:02 -0300

    commit inicial

---
+++ src/main/java/br/senai/lab365/sistema_medico/SistemaMedicoApplication.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.sistema_medico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/lab365/sistema_medico/SistemaMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.sistema_medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
